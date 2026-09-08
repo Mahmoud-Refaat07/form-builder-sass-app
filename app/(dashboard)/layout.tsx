5697c93ebcf46de105af5a405eaab87726c09e8c
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-09-09 01:41:16 +0200

    chore: initialize project setup

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,21 @@
+import Logo from "@/components/Logo";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
+import { SignIn, UserButton } from "@clerk/nextjs";
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
+      <nav className="flex justify-between items-center border-b border-border h-15 px-4 py-2">
+        <div className="min-w-47.5">
+          <Logo />
+        </div>
+        <div className="flex w-full gap-4 justify-end items-center ">
+          <ThemeSwitcher />
+          <UserButton afterSwitchSessionUrl="./sign-in" />
+        </div>
+      </nav>
+      <main className="flex w-full grow"> {children}</main>
+    </div>
+  );
+}
---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|png|jpg|jpeg|gif|svg|webp|ico|ttf|woff2?|eot|otf|pdf|txt|xml|csv|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
